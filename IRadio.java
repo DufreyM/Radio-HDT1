2868f86b26f6a97349061fb6f2cce434da3bc19f
Leonardo Mejía <123429700+DufreyM@users.noreply.github.com>
2024-01-10 08:46:21 -0600

    Interface IRadio

---
+++ IRadio.java
@@ -0,0 +1,13 @@
+/**
+ * IRadio
+ */
+public interface IRadio {
+
+    void saveStation(int buttonID, double station); 
+    boolean isAM();
+    boolean isOn();
+    double selectStation(int buttonID); 
+    void switchOnOff(); 
+    void switchAMFM(); 
+    double nextStation(); 
+}
